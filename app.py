02160fd692e804c752d69a3afb495d658396e2e2
masonnystrom <masonnystrom@gmail.com>
2020-04-30 15:37:30 -0400

    update app

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.feature_extraction.text import TfidfVectorizer
 import joblib
 
-HEROKU_BUILDPACK_GIT_LFS_REPO = "https://github.com/Build-Week-Medicine-Cabinet/DS/tree/master/pickled_files"
+# HEROKU_BUILDPACK_GIT_LFS_REPO = "https://github.com/Build-Week-Medicine-Cabinet/DS/tree/master/pickled_files"
 
 app = Flask(__name__)
 
